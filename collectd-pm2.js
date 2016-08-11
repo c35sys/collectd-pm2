4076b3fcf82cae4307213d19af7a9d05aadb8041
Christophe Le Guern <c35sys@gmail.com>
2016-08-11 15:24:20 +0200

    Refactoring for grafana usage
    - Use types.db entries for data types, used by tag (type_instance) in grafana 
    - Change names of values sent, with the following format : <instance-id>/<plugin>-<plugin_instance>/<type>-<type_instance>, see http://docs.rightscale.com/cm/rs101/create_custom_collectd_plug-ins_for_linux.html
    - Set PID number based on number of pid and not the PIDs themselves
    
    Example :
    PUTVAL "vm10l/nodejs-mycms/memory-memory_used-pid_3" interval=60 1470919497:165568512

--- collectd-pm2.js
+++ collectd-pm2.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 
 
 var hostname = process.env.COLLECTD_HOSTNAME || os.hostname();
-var interval = parseInt(process.env.COLLECTD_INTERVAL, 10) || 1;
+var interval = parseInt(process.env.COLLECTD_INTERVAL, 10) || 60;
 
 function collect () {
     exec('pm2 jlist', function (error, stdout, stderr) {
@@ -18,16 +18,28 @@ function collect () {
 
         var timestamp = Math.floor(Date.now() / 1000);
         var list = JSON.parse(stdout);
+        var oldname = '';
+        var pidnumber = 1;
+
         list.forEach(function (item) {
             var name = '';
             for(var i = 0, n = item.name.length; i < n; ++i) {
                 name += item.name[i].match(/^[0-9a-zA-Z]+$/) ? item.name[i] : '_';
             }
-            process.stdout.write("PUTVAL \"" + hostname + "/" + name + "-loop_delay" + "/delay-" + item.pm_id + "\" interval=" + interval + " " + timestamp + ":" + item.pm2_env.axm_monitor["Loop delay"].value.replace('ms', '') + "\n");
-            process.stdout.write("PUTVAL \"" + hostname + "/" + name + "-memory_used" + "/gauge-" + item.pm_id + "\" interval=" + interval + " " + timestamp + ":" + item.monit.memory + "\n");
-            process.stdout.write("PUTVAL \"" + hostname + "/" + name + "-cpu_used" + "/gauge-" + item.pm_id + "\" interval=" + interval + " " + timestamp + ":" + item.monit.cpu + "\n");
-            process.stdout.write("PUTVAL \"" + hostname + "/" + name + "-restart_time" + "/gauge-" + item.pm_id + "\" interval=" + interval + " " + timestamp + ":" + item.pm2_env.restart_time + "\n");
-            process.stdout.write("PUTVAL \"" + hostname + "/" + name + "-unstable_restarts" + "/gauge-" + item.pm_id + "\" interval=" + interval + " " + timestamp + ":" + item.pm2_env.unstable_restarts + "\n");
+            
+            // Increase number of pid per application, easier to process than pid defined by pm2
+            if (oldname != name) {
+                oldname = name;
+            }
+            else {
+                pidnumber = pidnumber + 1;
+            }
+
+            process.stdout.write("PUTVAL \"" + hostname + "/" + "nodejs-" + name + "/delay-" + "loop_delay-pid_" + pidnumber + "\" interval=" + interval + " " + timestamp + ":" + item.pm2_env.axm_monitor["Loop delay"].value.replace('ms', '') + "\n");
+            process.stdout.write("PUTVAL \"" + hostname + "/" + "nodejs-" + name + "/memory-" + "memory_used-pid_" + pidnumber + "\" interval=" + interval + " " + timestamp + ":" + item.monit.memory + "\n");
+            process.stdout.write("PUTVAL \"" + hostname + "/" + "nodejs-" + name + "/vcpu-" + "cpu_used-pid_" + pidnumber + "\" interval=" + interval + " " + timestamp + ":" + item.monit.cpu + "\n");
+            process.stdout.write("PUTVAL \"" + hostname + "/" + "nodejs-" + name + "/response_time-" + "restart_time-pid_" + pidnumber + "\" interval=" + interval + " " + timestamp + ":" + item.pm2_env.restart_time + "\n");
+            process.stdout.write("PUTVAL \"" + hostname + "/" + "nodejs-" + name + "/gauge-" + "unstable_restarts-pid_" + pidnumber + "\" interval=" + interval + " " + timestamp + ":" + item.pm2_env.unstable_restarts + "\n");
         });
 
         setTimeout(collect, interval*1000);
